cb9733026f3b65ad0bed3ad98206437608947c93
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2018-11-10 16:54:45 -0200

    add first tokens

---
+++ token/token.go
@@ -0,0 +1,35 @@
+// Package responsible to define all tokens present on sdsl
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and literals
+	IDENT = "IDENT" // variavel names
+	INT   = "INT"   // 12345
+
+	// Operators
+	ASSIGN  = "="
+	PLUS    = "+"
+	GET     = "get"
+	EXTRACT = "extract"
+	PARSE   = "parse"
+	ON      = "on"
+
+	// Delimiters
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+
+	// Keywords
+	xpath = "xpath"
+)
