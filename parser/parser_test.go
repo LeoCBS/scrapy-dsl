a1ac060843b54da6875b943ddf27588a18de2540
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2018-12-30 19:55:46 -0200

    start parser tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -3,18 +3,56 @@ package parser_test
 import (
 	"testing"
 
-	//	"github.com/LeoCBS/scrapy-dsl/ast"
+	"github.com/LeoCBS/scrapy-dsl/ast"
 	"github.com/LeoCBS/scrapy-dsl/lexer"
 	"github.com/LeoCBS/scrapy-dsl/parser"
 )
 
 func TestLetStatements(t *testing.T) {
 
-	input := "let leo = 10;"
+	input := "let leo = 10; let vivi = 10;"
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
 	if program == nil {
 		t.Error("parserProgram() return nil")
 	}
+	if len(program.Statements) != 3 {
+		t.Errorf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"leo"},
+		{"vivi"},
+	}
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+		return false
+	}
+	return true
 }
